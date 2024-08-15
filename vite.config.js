d5aaa9141ac57a3a598184b171088d85dd8c224e
bryanalay <bryanalmi27@hotmail.com>
2024-08-15 12:25:06 -0500

    idk

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://bryanalay.github.io/aalmiapp/dist',
   plugins: [react()]
 })
